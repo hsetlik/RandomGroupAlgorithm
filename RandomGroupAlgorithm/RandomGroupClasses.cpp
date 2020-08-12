eca6069578f619fed30847f0e00da06487fc1c86
hsetlik <haydensetlik@gmail.com>
2020-08-11 20:28:37 -0600

    Started filling in functions

--- RandomGroupAlgorithm/RandomGroupClasses.cpp
+++ RandomGroupAlgorithm/RandomGroupClasses.cpp
@@ -5,8 +5,39 @@
 //  Created by Hayden on 8/11/20.
 //  Copyright © 2020 Hayden Setlik. All rights reserved.
 //
-
 #include "RandomGroupClasses.hpp"
 #include <cmath>
+
+Space::Space() {
+}
+
+Space:: ~Space(){
+    
+}
+
+void Space::init(int x, int y, int indexChoice){
+    xPos = x;
+    yPos = y;
+    index = indexChoice;
+    inGroup = false;
+}
+
+Grid::Grid(){
+    
+}
+Grid::~Grid(){
+}
+
+void Grid::init(int width, int height){
+    numIndeces = 256;
+    int currentIndex = 0;
+    for(int x = 0; x < width; x++){
+        for(int y = 0; y < height; y++){
+            memberSpaces[x][y].init(x, y, currentIndex);
+            currentIndex++;
+        }
+    }
+}
+
 
 
--- RandomGroupAlgorithm/RandomGroupClasses.hpp
+++ RandomGroupAlgorithm/RandomGroupClasses.hpp
@@ -15,23 +15,35 @@ class Space{
 public:
     Space();
     ~Space();
-    void init(int x, int y);
+    void init(int x, int y, int index);
     int xPos;
     int yPos;
+    int index;
+    bool inGroup;
 };
 class Grid{
 public:
     Grid();
     ~Grid();
     void init(int width, int height);
+    static int numIndeces;
+    int width;
+    int height;
+    Space memberSpaces[16][16];
+    int memberIndeces[256];
     bool areAdjacent(Space spaceA, Space spaceB);
 };
 class Group{
 public:
     Group();
     ~Group();
-    
+    Grid workingGrid;
+    void init(Space *firstSpace, Grid grid);
+    void addSpace(int *indexList);
+    void updateOptions();
+   
+private:
+    Space *_memberSpaces[15];
+    static int memberIndeces[15];
 }
-
-
 #endif /* RandomGroupClasses_hpp */
