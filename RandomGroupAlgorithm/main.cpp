89757d97b3c770faf8c3d53a483f13bc3bac31ce
hsetlik <haydensetlik@gmail.com>
2020-08-11 19:52:03 -0600

    Initial Commit

---
+++ RandomGroupAlgorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RandomGroupAlgorithm
+//
+//  Created by Hayden on 8/11/20.
+//  Copyright © 2020 Hayden Setlik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
